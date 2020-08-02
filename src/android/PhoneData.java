8dcae7b7be6fbdc2f1f89e94659b4023f70771e4
Judik <InkognitoZum@gmail.com>
2020-08-02 12:25:53 +0300

    add logic get phone number

--- src/android/PhoneData.java
+++ src/android/PhoneData.java
@@ -16,27 +16,86 @@ import org.json.JSONObject;
 /**
  * This class echoes a string called from JavaScript.
  */
-public class PhoneData extends CordovaPlugin {
+public class PhoneData extends CordovaPlugin implements ActivityCompat.OnRequestPermissionsResultCallback  {
 
+        private static final int PERMISSION_READ_STATE = 0;
+    private static final int PERMISSION_READ_SMS = 1;
+    private static final int PERMISSION_READ_PHONE_NUMBERS = 2;
+    private static boolean isReadPhoneState = false;
+    private static boolean isReadSMS = false;
+    private static boolean isReadPhoneNumbers = false;
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if (action.equals("getData")) {
+            this.getData(callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
+    private void getData(CallbackContext callbackContext) {
         Context context = this.cordova.getActivity().getApplicationContext();
-        TelephonyManager tMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.READ_PHONE_STATE) ==
+                PackageManager.PERMISSION_GRANTED) {
+            isReadPhoneState = true;
+        } else {
+            ActivityCompat.requestPermissions(this.cordova.getActivity(), new String[]{Manifest.permission.READ_PHONE_STATE}, PERMISSION_READ_STATE);
         }
-        String mPhoneNumber = tMgr.getLine1Number();
-        callbackContext.success(mPhoneNumber);
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.READ_SMS) ==
+                PackageManager.PERMISSION_GRANTED) {
+            isReadSMS = true;
+        } else {
+            ActivityCompat.requestPermissions(this.cordova.getActivity(), new String[]{Manifest.permission.READ_SMS}, PERMISSION_READ_SMS);
+        }
+        if (ContextCompat.checkSelfPermission(
+                context, Manifest.permission.READ_PHONE_NUMBERS) ==
+                PackageManager.PERMISSION_GRANTED) {
+            isReadPhoneNumbers = true;
+        } else {
+            ActivityCompat.requestPermissions(this.cordova.getActivity(), new String[]{Manifest.permission.READ_PHONE_NUMBERS}, PERMISSION_READ_PHONE_NUMBERS);
+        }
+        if (isReadPhoneNumbers && isReadSMS && isReadPhoneState) {
+            TelephonyManager tMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            String mPhoneNumber = tMgr.getLine1Number();
+            callbackContext.success(mPhoneNumber);
+        } else {
+            callbackContext.error("One of necessary permissions denied!");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_READ_STATE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isReadPhoneState = true;
+                } else {
+                    isReadPhoneState = false;
+                }
+                break;
+            }
+            case PERMISSION_READ_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isReadSMS = true;
+                } else {
+                    isReadSMS = false;
+                }
+                break;
+            }
+            case PERMISSION_READ_PHONE_NUMBERS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isReadPhoneNumbers = true;
+                } else {
+                    isReadPhoneNumbers = false;
+                }
+                break;
+            }
+
+        }
     }
 }
